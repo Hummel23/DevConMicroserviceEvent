8e6664c460f83253f3584f1ea8f3d6e282e731e9
Saba <nigistsaba@hotmail.com>
2016-10-21 17:17:25 +0200

    Event test class created, dao package added

--- src/main/java/com/senacor/model/Event.java
+++ src/main/java/com/senacor/model/Event.java
@@ -1,7 +1,25 @@
 package com.senacor.model;
 
+import java.sql.Time;
+import java.util.Date;
+
 /**
  * Created by saba on 21.10.16.
  */
 public class Event {
+    Date date;
+
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+
+
+
 }
---
+++ src/test/java/com/senacor/model/EventTest.java
@@ -0,0 +1,23 @@
+package com.senacor.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by saba on 21.10.16.
+ */
+public class EventTest {
+
+
+    @Test
+    public void getDate() throws Exception {
+
+    }
+
+    @Test
+    public void setDate() throws Exception {
+
+    }
+
+}

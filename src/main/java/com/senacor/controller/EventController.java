5ce33e58d47c4b98db5c0a017961d045cd56a6fb
s0549129 <s0549129@htw-berlin.de>
2016-10-24 20:34:54 +0200

    Angepasster EventController - mit Fehlermeldung bei Postmaster

--- src/main/java/com/senacor/controller/EventController.java
+++ src/main/java/com/senacor/controller/EventController.java
@@ -3,11 +3,9 @@ package com.senacor.controller;
 import com.senacor.model.Event;
 import com.senacor.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by saba on 21.10.16.
@@ -28,4 +26,29 @@ public class EventController {
         public Event sgetEvent(@PathVariable("id") String id){
         return eventRepository.findOne(id);
     }
+
+    //Create Event Object with parameters with http-POST Request
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createEvent(@RequestBody Event event) {
+        Event createdEvent = new Event();
+        createdEvent.setName(event.getName());
+        createdEvent.setPlace(event.getPlace());
+        //createdEvent.setDate();
+        eventRepository.save(createdEvent);
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateEvent(@PathVariable("id") String id, @RequestBody Event event) {
+        Event existingEvent = eventRepository.findOne(id);
+        existingEvent.setName(event.getName());
+        existingEvent.setPlace(event.getPlace());
+        existingEvent.setDate(event.getDate());
+        eventRepository.save(existingEvent);
+    }
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE) //headers = "Accept=application/json"
+    public void deleteEvent(@PathVariable("id") String id) {
+        eventRepository.delete(id);
+    }
+
 }

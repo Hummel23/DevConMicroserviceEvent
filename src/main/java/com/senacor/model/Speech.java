baa413521d6884a203f36d7420784dea9bd4a0ac
s0549129 <s0549129@htw-berlin.de>
2016-10-27 16:50:58 +0200

    Speech Model Class created - Links Testen Controller hinzugefügt

--- src/main/java/com/senacor/model/Speech.java
+++ src/main/java/com/senacor/model/Speech.java
@@ -1,9 +1,76 @@
 package com.senacor.model;
 
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
 /**
  * Created by saba on 21.10.16.
  */
 
 public class Speech {
-    private String speechName;
+
+    @Id
+    private String id;
+    private String speechTitle;
+    private LocalDate speechTime;
+    /*
+    Objekte werden dann von Werten wie folgt gebildet:LocalDate.of(2012, Month.DECEMBER, 12); // from values
+     */
+    private String speechLocation;
+    private String speechSpeaker;
+    private String speechSummary;
+    private String speechResumeSpeaker;
+
+    public Speech() {
+    }
+
+    public String getSpeechTitle() {
+        return speechTitle;
+    }
+
+    public void setSpeechTitle(String speechTitle) {
+        this.speechTitle = speechTitle;
+    }
+
+    public LocalDate getSpeechTime() {
+        return speechTime;
+    }
+
+    public void setSpeechTime(LocalDate speechTime) {
+        this.speechTime = speechTime;
+    }
+
+    public String getSpeechLocation() {
+        return speechLocation;
+    }
+
+    public void setSpeechLocation(String speechLocation) {
+        this.speechLocation = speechLocation;
+    }
+
+    public String getSpeechSpeaker() {
+        return speechSpeaker;
+    }
+
+    public void setSpeechSpeaker(String speechSpeaker) {
+        this.speechSpeaker = speechSpeaker;
+    }
+
+    public String getSpeechSummary() {
+        return speechSummary;
+    }
+
+    public void setSpeechSummary(String speechSummary) {
+        this.speechSummary = speechSummary;
+    }
+
+    public String getSpeechResumeSpeaker() {
+        return speechResumeSpeaker;
+    }
+
+    public void setSpeechResumeSpeaker(String speechResumeSpeaker) {
+        this.speechResumeSpeaker = speechResumeSpeaker;
+    }
+
 }

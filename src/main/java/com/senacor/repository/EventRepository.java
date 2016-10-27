5368ed9f6f60c2efa73a1baa5a5028a8cddb8f20
Saba <nigistsaba@hotmail.com>
2016-10-27 23:08:03 +0200

    eventrepository and controller adapted for test purposes/GET method returns one event only

--- src/main/java/com/senacor/repository/EventRepository.java
+++ src/main/java/com/senacor/repository/EventRepository.java
@@ -3,6 +3,7 @@ package com.senacor.repository;
 import com.senacor.model.Event;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RepositoryRestResource(collectionResourceRel = "event", path = "event")
 public interface EventRepository extends MongoRepository<Event, String> {
+
+    Event findByName(@Param("name") String name);
 }

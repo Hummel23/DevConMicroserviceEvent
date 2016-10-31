72179f7d5c34da2e2ab4635fcd992522852e2f72
Saba <nigistsaba@hotmail.com>
2016-10-31 16:25:45 +0100

    tries to implement speech model and speechrepository

---
+++ src/main/java/com/senacor/repository/SpeechRepository.java
@@ -0,0 +1,20 @@
+package com.senacor.repository;
+
+import com.senacor.model.Speech;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by saba on 31.10.16.
+ */
+
+@Repository
+@RepositoryRestResource(collectionResourceRel = "speech", path = "speech")
+public interface SpeechRepository extends MongoRepository<Speech, String> {
+
+    List<Speech> findByEventId(@Param("eventId") String eventId);
+}

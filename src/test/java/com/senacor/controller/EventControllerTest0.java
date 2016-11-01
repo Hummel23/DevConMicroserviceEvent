fe0708dc2e4b48602da7f3572760eaf06561b06b
s0549129 <s0549129@htw-berlin.de>
2016-11-01 14:10:03 +0100

    EventControllerTest - added getEventSpeeches test method

--- src/test/java/com/senacor/controller/EventControllerTest0.java
+++ src/test/java/com/senacor/controller/EventControllerTest0.java
@@ -1,22 +1,32 @@
 package com.senacor.controller;
 
+import com.senacor.model.Speech;
 import com.senacor.repository.EventRepository;
+import com.senacor.repository.SpeechRepository;
+import com.sun.tools.javac.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.boot.autoconfigure.web.WebMvcProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 import org.springframework.web.servlet.View;
 
+import java.util.*;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 /**
@@ -29,6 +39,9 @@ public class EventControllerTest0 {
 
     @Mock
     EventRepository eventRepository;
+
+    @Mock
+    SpeechRepository speechRepository;
 
     @Mock
     View mockView;
@@ -52,6 +65,22 @@ public class EventControllerTest0 {
         .andExpect(status().isOk());
 
     }
+
+    @Test
+    public void getEventSpeeches() throws Exception {
+        ArrayList<Speech> speech = new ArrayList<>();
+        speech.add(new Speech("ID"));
+        when(createEventControllerMock.getEventSpeeches("eventID")).thenReturn(speech);
+        mockMvc.perform(get("event/eventID"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("eventID", speech))
+                .andExpect(view().name("eventID"));
+}
+
+
+
+
+
 
 
 }

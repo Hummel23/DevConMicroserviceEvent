0383edf92f681840cec819a7c78d0a0dbda7b057
matulja <maryna.fehler@gmail.com>
2016-11-05 13:27:22 +0100

    getEvent klasse with Array and response

--- src/test/java/com/senacor/controller/EventControllerTest0.java
+++ src/test/java/com/senacor/controller/EventControllerTest0.java
@@ -4,33 +4,24 @@ import com.senacor.model.Event;
 import com.senacor.model.Speech;
 import com.senacor.repository.EventRepository;
 import com.senacor.repository.SpeechRepository;
-import com.sun.tools.javac.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.web.WebMvcProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 import org.springframework.web.servlet.View;
 
-import java.util.*;
+import java.util.ArrayList;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
 
 /**
  * Created by Marynasuprun on 31.10.2016.
@@ -63,9 +54,21 @@ public class EventControllerTest0 {
     }
     @Test
     public void getEvent() throws Exception {
+        ArrayList<Event> list = new ArrayList<>();
+        list.add(new Event("id"));
+        list.add(new Event("id"));
+        String expectedData= "Conference No.1";
+        when(createEventControllerMock.listAllEvents()).thenReturn(list);
+                mockMvc.perform(get("/event").contentType(MediaType.APPLICATION_JSON_UTF8))
 
-        mockMvc.perform(get("/event"))
-        .andExpect(status().isOk());
+                        .andExpect(jsonPath("$.[0].name", is("id")))
+
+                        //Array now,  but wait for Json Object
+                        //.andExpect(MockMvcResultMatchers.jsonPath("$.name").value(expectedData))
+
+                        .andExpect(status().isOk());
+
+
 
     }
 

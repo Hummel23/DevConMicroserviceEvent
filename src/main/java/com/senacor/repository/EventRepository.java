8324548efa3ee70bb9216b6ca147c80162051781
s0549129 <s0549129@htw-berlin.de>
2016-11-28 21:09:20 +0100

    adapted classes during code review with Senacor - Event Repository

--- src/main/java/com/senacor/repository/EventRepository.java
+++ src/main/java/com/senacor/repository/EventRepository.java
@@ -1,25 +1,21 @@
 package com.senacor.repository;
 
 import com.senacor.model.Event;
-import com.senacor.model.Speech;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 /**
  * Created by Nadia on 23.10.2016.
  */
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "event", path = "event")
+//@RepositoryRestResource(collectionResourceRel = "event", path = "event")
 public interface EventRepository extends MongoRepository<Event, String> {
 
     //Event findByEventId(@Param("eventId") String eventId);
+    Event findAllOrderByDate(); //Comparable Interface wieder rausnehmen Event-Klasse
+
+    //find all / orderBy nachschlagen - in Mongo direkt sortieren
+    //Event Repository implement Klasse
 
 }

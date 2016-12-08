f6fb076485e244585a9ddf07743d3a2a64791927
matulja <maryna.fehler@gmail.com>
2016-12-08 22:07:01 +0100

    updates for testing and post method

--- src/main/java/com/senacor/controller/EventController.java
+++ src/main/java/com/senacor/controller/EventController.java
@@ -2,14 +2,16 @@ package com.senacor.controller;
 
 import com.senacor.model.Event;
 import com.senacor.model.Speech;
-import com.senacor.service.EventService;
 import com.senacor.service.AuthenticationService;
+import com.senacor.service.EventService;
+import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by saba on 21.10.16.
@@ -28,7 +30,6 @@ public class EventController {
         this.eventService = eventService;
         this.authenticationService = authenticationService;
     }
-
 
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
@@ -71,34 +72,52 @@ public class EventController {
         return eventService.getSpeech(eventID, speechID);
     }
 
-    //Speeches anlegen - insertSort beim Post/Put durchführen - comparable Interface bei Speeches wegfallen lassen
+    @RequestMapping(value = "/createEvent", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Event createEvent(@RequestBody (required = false) Event event) {
 
-/*    //Create Event Object with parameters with http-POST Request
-    @RequestMapping(value = "", method = RequestMethod.POST)
+        Event createdEvent = new Event();
+        createdEvent.setName("Conference 10");
+        createdEvent.setEventId(UUID.randomUUID().toString());
+        createdEvent.setPlace("Example Street No. 10");
+        createdEvent.setDate(new LocalDate(2017, 8, 1));
+
+        return eventService.createEvent(createdEvent);
+
+    }
+
+    //Speeches anlegen - insertSort beim Post/Put durchführen - comparable Interface bei Speeches wegfallen lassen
+    //Create Event Object with parameters with http-POST Request
+
+    /*@RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     public void createEvent(@RequestBody Event event) {
         Event createdEvent = new Event();
-        createdEvent.setName(event.getName());#
+        createdEvent.setName(event.getName());
         createdEvent.setEventId(UUID.randomUUID().toString());
         createdEvent.setPlace(event.getPlace());
         //createdEvent.setDate();
         eventRepository.save(createdEvent);
-    }*/
-/*    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    } */
+
+
+   /* @RequestMapping(value = "/{eventId}", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateEvent(@PathVariable("id") String id, @RequestBody Event event) {
+    public void updateEvent(@PathVariable("eventId") String eventId,
+                            @RequestBody (required=false) Event event) {
         Event existingEvent = eventRepository.findOne(id);
         existingEvent.setName(event.getName());
         existingEvent.setPlace(event.getPlace());
-       // existingEvent.setDate(event.getDate());
+        // existingEvent.setDate(event.getDate());
         eventRepository.save(existingEvent);
-    }
-    */
+    }*/
+
 
     @RequestMapping(value = "/{eventId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEvent(@PathVariable("eventId") String eventId) {
-        eventService.deleteEvent(eventId);
+       eventService.deleteEvent(eventId);
     }
 
 }
+
--- src/test/java/com/senacor/controller/EventControllerTest.java
+++ src/test/java/com/senacor/controller/EventControllerTest.java
@@ -3,8 +3,9 @@ package com.senacor.controller;
 
 import com.senacor.model.Event;
 import com.senacor.model.Speech;
-import com.senacor.service.EventService;
 import com.senacor.service.AuthenticationService;
+import com.senacor.service.EventService;
+import org.joda.time.LocalDate;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -12,11 +13,15 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.startsWith;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
-
 
 /**
  * Created by Marynasuprun on 31.10.2016.
@@ -36,11 +41,12 @@ public class EventControllerTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        createEventController= new EventController(eventService, authenticationService);
+        createEventController = new EventController(eventService, authenticationService);
     }
 
+    // Test listAllEvents
     @Test
-    public void getEvent() throws Exception {
+    public void getEvents() throws Exception {
 
         ArrayList<Event> list = new ArrayList<>();
         Event event1 = new Event();
@@ -51,6 +57,7 @@ public class EventControllerTest {
         event1.getSpeeches().add(speech1);
         event1.getSpeeches().add(speech2);
         list.add(event1);
+
         Event event2 = new Event();
         event2.setName("Conference2");
         list.add(event2);
@@ -58,14 +65,13 @@ public class EventControllerTest {
 
         when(eventService.listAllEvents()).thenReturn(list);
         List<Event> events = createEventController.listAllEvents();
-        assertThat(events,hasSize(2));
-
-        verify(eventService,times(1)).listAllEvents();
+        assertThat(events, hasSize(2));
+        assertThat(events, hasItem(event1));
+        verify(eventService, times(1)).listAllEvents();
 
     }
 
-    //Linklogik noch testen
-
+    //Test getSpeeches for Event
     @Test
     public void getEventSpeeches() throws Exception {
 
@@ -76,26 +82,80 @@ public class EventControllerTest {
 
         Speech speech1 = new Speech(event1.getEventId());
         Speech speech2 = new Speech(event1.getEventId());
-
         speech1.setSpeaker("Dr. Obermann");
         speech1.setSpeechRoom("Nr: 234");
-
         event1.getSpeeches().add(speech1);
         event1.getSpeeches().add(speech2);
-
         list.add(event1);
 
+
         when(eventService.getSpeech(event1.getEventId(), speech1.getSpeechId())).thenReturn(speech1);
+        //initialise new Speech for Test
         Speech speech = eventService.getSpeech(event1.getEventId(), speech1.getSpeechId());
 
-        assertThat(speech1.getSpeaker(), true);
-
-
+        assertThat(speech.getSpeaker(), true);
+        assertThat(event1.getSpeeches(), hasSize(2));
+        assertThat(speech.getSpeaker(), startsWith("Dr."));
         verify(eventService, times(1)).getSpeech(event1.getEventId(), speech1.getSpeechId());
 
     }
 
+    // Test CreateEvent
+    @Test
+    public void createEvent() throws Exception {
+
+        Event event1 = new Event();
+        event1.setName("Conference 20");
+        event1.setEventId(UUID.randomUUID().toString());
+        event1.setPlace("Example Street No. 20");
+        event1.setDate(new LocalDate(2017, 6, 1));
+
+        when(eventService.createEvent(event1)).thenReturn(event1);
+        Event event = createEventController.createEvent(event1);
+
+        //Object is not null, event is created
+        assertNotNull(event);
+        assertNotNull(event.getSpeeches());
+        assertTrue(event.getPlace()=="Example Street No. 20");
+        assertEquals("Conference 20", event.getName());
+        verify(eventService, times(1)).createEvent(event);
+
+
+
+    }
+
+    //deleteEvent
+
+    @Test
+    public void deleteEvent() throws Exception {
+
+        Event event1 = new Event();
+        event1.setName("Conference 1");
+        event1.setEventId(UUID.randomUUID().toString());
+        Event event2 = new Event();
+        event2.setName("Conference 2");
+        event2.setEventId(UUID.randomUUID().toString());
+
+
+        when(eventService.getEvent(event1.getEventId())).thenReturn(event1);
+        Event event=createEventController.getEvent(event1.getEventId());
+
+       // TODO event should be deleted
+       // eventService.deleteEvent(event.getEventId());
+        createEventController.deleteEvent(event.getEventId());
+
+        verify(eventService, times(1)).getEvent(event.getEventId());
+        verify(eventService, times(1)).deleteEvent(event.getEventId());
+
+        System.out.println("Deleted Event " +event.getEventId());
+
+    }
+
+    //Linklogik noch testen
 
 }
+
+
+
 
 

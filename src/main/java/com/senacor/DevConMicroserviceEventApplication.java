24c4a143994bbc0c61f76a0709007e9dd908f122
Saba <nigistsaba@hotmail.com>
2017-01-08 18:57:28 +0100

    with code to create events in past - for testing editing of past events

--- src/main/java/com/senacor/DevConMicroserviceEventApplication.java
+++ src/main/java/com/senacor/DevConMicroserviceEventApplication.java
@@ -4,6 +4,7 @@ import com.senacor.model.Event;
 import com.senacor.model.Speech;
 import com.senacor.repository.EventRepository;
 import com.senacor.service.EventService;
+import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -32,5 +33,33 @@ public class DevConMicroserviceEventApplication implements CommandLineRunner{
 
         //eventRepository.deleteAll();
 
+
+   /*     for (Event event :
+                eventRepository.findAll()) {
+            if(! event.getDate().isAfter(LocalDate.now())){
+                eventRepository.delete(event);
+            }
+        }
+        Event event = new Event();
+        event.setDate(new LocalDate(2015, 03, 02));
+        event.setPlace("Berlin");
+        event.setName("Event in der Vergangenheit");
+        eventRepository.save(event);
+
+        Event event1 = new Event();
+        event1.setDate(LocalDate.now());
+        event1.setPlace("Berlin");
+        event1.setName("heutiges");
+        eventRepository.save(event1);
+
+        Event oldestEvent = eventRepository.findAllByOrderByDateDesc().get(eventRepository.findAll().size()-1);
+
+        Speech speech = new Speech();
+        speech.setSpeechTitle("past speeches cannot be amended");
+        speech.setStartTime(LocalTime.of(14, 00));
+        speech.setEndTime(LocalTime.of(15, 30));
+
+        oldestEvent.getSpeeches().add(speech);
+        eventRepository.save(oldestEvent);*/
     }
 }

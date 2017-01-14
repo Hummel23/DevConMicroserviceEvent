890e3b388ad5793a5594b2d783c1d4e7663da615
Saba <nigistsaba@hotmail.com>
2017-01-14 14:36:43 +0100

    further implementation of interface for communication with rating service

--- src/main/java/com/senacor/model/Speech.java
+++ src/main/java/com/senacor/model/Speech.java
@@ -1,5 +1,6 @@
 package com.senacor.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
